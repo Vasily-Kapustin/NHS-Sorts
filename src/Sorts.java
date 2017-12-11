5c2f03fecfa37ac7532c60ae784443a2f84e0f22
073752701 <073752701@109R0305737CQ22.EDU.YRDSB.NET>
2017-12-11 11:11:05 -0500

    This is old

---
+++ src/Sorts.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Zhaojingyu
+ */
+public class Sorts {
+    public static Comparable[] selectionSort(Comparable[] list) {
+        for (int i = 0; i < list.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < list.length; j++) {
+                if (list[j].compareTo(list[minIndex]) < 0) {
+                    minIndex = j;
+                }
+
+            }
+            Comparable temp = list[i];
+            list[i] = list[minIndex];
+            list[minIndex] = temp;
+        }
+        return list;
+    }
+    /** Sort and array using insertionSort
+     * Parse through each data and move it back until there is a smaller value before it
+     * @param list A Comparable list which is going to be sorted
+     * @return sorted array 
+     */
+    public static Comparable[] insertionSort(Comparable[] list) {
+        for (int i = 1; i < list.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (list[j].compareTo(list[j - 1])<0) {
+                    Comparable temp = list[j];
+                    list[j] = list[j - 1];
+                    list[j - 1] = temp;
+                }
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Using a bubble sort algorithm, sort a a comparable array.
+     *
+     * @param list The array to sort.
+     * @return The sorted array.
+     */
+    public static Comparable[] bubbleSort(Comparable[] list) {
+        //Track whether swaps were made
+        boolean swap;
+        //Temporary variable used when swapping elements
+        Comparable temp;
+        do {
+            //No swap was made
+            swap = false;
+            //Loop through the array
+            for (int i = 0; i < list.length; i++) {
+                //Compare the element and the element above it
+                try {
+                    if (list[i].compareTo(list[i + 1]) > 0) {
+                        //Swap the two elements
+                        temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                        //Swap was made
+                        swap = true;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                }
+            }
+        } while (swap);
+        return list;
+    }
+}
